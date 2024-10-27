38f2200f28c5319869a41224ec9d67f8adc77162
Jacob Craven <jcraven@siue.edu>
2024-10-26 22:16:20 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Navbar from './components/Navbar.svelte';
+  import './styles/global.css'
+  import './styles/fonts.css'
+  import { Router } from 'svelte-routing';
+
+  const links = [
+          { name: 'Home', href: '/' },
+          { name: 'About', href: '/about' },
+          { name: 'Contact', href: '/contact' }
+  ];
+
+</script>
+
+<Navbar {links} />
+
+<!--<Router>-->
+<!--  <Home path="/" />-->
+<!--  <About path="/about" />-->
+<!--  <Projects path="/projects" />-->
+<!--  <Contact path="/contact" />-->
+<!--</Router>-->
+
+<main>
+  <h1>Welcome to my Svelte app!</h1>
+  <p>This is a simple Svelte app with routing.</p>
+</main>
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABV-h1rwrf4HNzB38pB_Uaak6PUEBbw_Q",
+  authDomain: "jacobcraven-7c2f6.firebaseapp.com",
+  projectId: "jacobcraven-7c2f6",
+  storageBucket: "jacobcraven-7c2f6.appspot.com",
+  messagingSenderId: "394892556373",
+  appId: "1:394892556373:web:bff21d6171c874fe8ebcdc",
+  measurementId: "G-JNHQGHR6P5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
